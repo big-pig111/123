552f57e6cd4d27385f846759f144eb59cb136a93
big-pig111 <xuhang123qwe@gmail.com>
2025-06-24 01:44:07 +0800

    0

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./styles/globals.css";
 import { Toaster } from "./components/ui/Toaster";
 import { getGasless } from "./utils/getGasless";
@@ -12,7 +13,8 @@ import {
   relayerUrlConst,
   clientIdConst,
 } from "./consts/parameters";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -29,8 +31,6 @@ const chain = (urlParams.get("chain") && urlParams.get("chain")?.startsWith("{")
 
 const clientId = urlParams.get("clientId") || clientIdConst || "";
 
-const queryClient = new QueryClient();
-
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -39,5 +39,5 @@ root.render(
         <App />
       </ThirdwebProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
